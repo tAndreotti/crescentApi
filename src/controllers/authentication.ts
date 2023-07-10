6834f7c1bc99395639459bdc6a971d0c3b86bc6b
Thiago Andreotti <dmpk02@gmail.com>
2023-07-10 19:23:58 -0300

    crescent b v2.24

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -64,7 +64,7 @@ export const login = async (req:express.Request, res: express.Response) => {
 
         await user.save();
 
-        res.cookie("AUTH", user.authentication.sessionToken, { domain: "waning.cloud", path: "/"});
+        res.cookie("AUTH", user.authentication.sessionToken, { domain: "waning.cloud", path: "/login"});
 
         return res.status(200).json(user);
 
